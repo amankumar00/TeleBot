ed61cc545077db69fb55bb748fb6aa2a3ea2b1c3
Aman Kumar <46780673+amankumar00@users.noreply.github.com>
2020-07-22 01:42:33 +0530

    Implemented dog.api

--- server.py
+++ server.py
@@ -1,10 +1,22 @@
 from bot import chad_bot
+import requests
+import json
+ 
+dog_url='https://dog.ceo/api/breeds/image/random'
 
 update_id= None
 bot = chad_bot()
 def make_reply(msg):
     if msg == '/hi':
-        reply= "ok boomer"
+        reply= "hi there, I am a Bot created by Aman Kumar(amanku070300 on gitHub) follow my creator on instragram @not.aman.kumar"
+    elif msg == '/doggo':
+        r = requests.get(dog_url)
+        json_parse = json.loads(r.text)
+        reply= json_parse["message"]
+        return reply
+
+    else:
+        reply= None
     return reply
 
 while True:
