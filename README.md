4d2322e9b7f1f92933138fe598c8cd2faf667bd3
Aman Kumar <46780673+amankumar00@users.noreply.github.com>
2020-07-22 03:24:32 +0530

    bug fix

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # TeleBot
+Hi!!! I am a bot created by Aman Kumar(github:https://github.com/amankumar00).
+                    Currently you can use following commands:
+                    - /help - list  all the commands.
+                    - /hi - A hello greeting.
+                    - /doggo - I will send you a random image of a dog <3.
--- server.py
+++ server.py
@@ -11,16 +11,17 @@ def make_reply(msg):
         reply= "hi there, I am a Bot created by Aman Kumar(amanku070300 on gitHub) follow my creator on instragram @not.aman.kumar"
     elif msg == '/doggo':
         r = requests.get(dog_url)
-        json_parse = json.load(r.text)
+        json_parse = json.loads(r.text)
         reply= json_parse["message"]
         return reply
     elif msg == '/start' or '/help':
-        reply = '''Hi!!! I am a bot created by Aman Kumar(github:https://github.com/amankumar00).
-                    Currently you can use following commands:
-                    - /help - list  all the commands.
-                    - /hi - A hello greeting.
-                    - /doggo - I will send you a random image of a dog <3.
-                    '''
+        reply = '''
+        Hi!!! I am a bot created by Aman Kumar(github:https://github.com/amankumar00).
+        Currently you can use following commands:
+            - /help - list  all the commands.
+            - /hi - A hello greeting.
+            - /doggo - I will send you a random image of a dog <3.
+            '''
 
     else:
         reply= None
